1aa5886311a8571afd5952cd3f316ac8e5c8af52
Bob Ansari <babakansari@hotmail.com>
2021-08-05 17:58:09 -0400

    Copy assemblies to the container for executing the tests

--- SeleniumTestsOnContainers/RunTests.ps1
+++ SeleniumTestsOnContainers/RunTests.ps1
@@ -1,5 +1,7 @@
-﻿# dotnet test bin/Debug/netcoreapp3.1/SeleniumTestsOnContainers.dll --logger:junit;LogFileName=bin/Debug/netcoreapp3.1/Intelex.Automation.DataImport-results.xml  -v:n
+﻿# dotnet test bin/Debug/net5.0/SeleniumTestsOnContainers.dll --logger:junit;LogFileName=bin/Debug/netcoreapp3.1/Intelex.Automation.DataImport-results.xml  -v:n
 
-dotnet test bin/Debug/netcoreapp3.1/SeleniumTestsOnContainers.dll
+# Run in the host
+# dotnet test bin/Debug/net5.0/SeleniumTestsOnContainers.dll
 
-
+# Run in the container
+dotnet test SeleniumTestsOnContainers.dll 
